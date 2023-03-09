0673fc8bcda470199b9975c5ea2925c900ab06a7
Fatima-hub333 <89512061+Fatima-hub333@users.noreply.github.com>
2023-03-09 23:56:55 +0500

    change prices

--- src/redux/homepage/homeslice.js
+++ src/redux/homepage/homeslice.js
@@ -3,40 +3,40 @@ import { createSlice } from '@reduxjs/toolkit'
 const initialState = {
  floorData:{
     floor:"First Floor",
-    square:56500,
+    square:58000,
     shop:"F1",
-    shopFeet:714.12,
-    totalPrice:43151875
+    shopFeet:763.75,
+    totalPrice:44297500
   },
   GroundFloor:[
     {
       floor:"Ground Floor",
-      square: 95000,
+      square: 99000,
       shop:"G1",
-      shopFeet:652.56 ,
-      totalPrice:61993200
+      shopFeet:692.50 ,
+      totalPrice:68557500
     },
     {
       floor:"Ground Floor",
-      square:95000,
+      square:99000,
       shop:"G2",
-      shopFeet:639.31 ,
-      totalPrice: 60734450,
+      shopFeet:651.25 ,
+      totalPrice: 64473750,
       sold:true,
     },
     {
       floor:"Ground Floor",
-      square:95000 ,
+      square:99000 ,
       shop:"G3",
-      shopFeet:639.31 ,
-      totalPrice: 60734450
+      shopFeet:651.25 ,
+      totalPrice: 64473750
     },
     {
       floor:"Ground Floor",
-      square:95000,
+      square:99000,
       shop:"G4",
-      shopFeet:612.81  ,
-      totalPrice: 40751865,
+      shopFeet:692.50  ,
+      totalPrice: 68557500,
 
     }
   ],
@@ -44,127 +44,127 @@ const initialState = {
 
       {
         floor:"Lower Ground",
-        square:66500,
+        square:68500,
         shop:"L1",
-        shopFeet:652.56 ,
-        totalPrice:43395240
+        shopFeet:658.33 ,
+        totalPrice:45095605
       },
       {
         floor:"Lower Ground",
-        square:66500,
+        square:68500,
         shop:"L2",
-        shopFeet:639.31 ,
-        totalPrice: 42514115
+        shopFeet:620.83 ,
+        totalPrice: 42526855
       },
       {
         floor:"Lower Ground",
-        square:66500,
+        square:68500,
         shop:"L3",
-        shopFeet:639.31 ,
-        totalPrice: 42514115
+        shopFeet:620.83 ,
+        totalPrice: 42526855
       },
       {
         floor:"Lower Ground",
-        square:66500,
+        square:68500,
         shop:"L4",
         shopFeet:612.81  ,
-        totalPrice: 40751865,
+        totalPrice: 73608730,
         sold:true
       }
   ],
   Mezzanine:[
     {
       floor:"Mezzanine",
-      square: 71500,
+      square: 73500,
       shop:"M1", 
-      shopFeet:714.12 ,
-      totalPrice:51059580,
+      shopFeet:763.75 ,
+      totalPrice:56135625,
       sold:true,
     },
     {
       floor:"Mezzanine",
-      square:71500,
+      square:73500,
       shop:"M2",
-      shopFeet:699.62,
-      totalPrice:50022830,
+      shopFeet:714.37,
+      totalPrice:52506195,
     },
     {
       floor:"Mezzanine",
-      square:71500,
+      square:73500,
       shop:"M3",
-      shopFeet:699.62,
-      totalPrice: 50022830
+      shopFeet:714.37,
+      totalPrice: 52506195
     },
     {
       floor:"Mezzanine",
-      square:71500,
+      square:73500,
       shop:"M4",
-      shopFeet:670.62,
-      totalPrice: 47949330,
+      shopFeet:763.75,
+      totalPrice: 56135625,
     }
   ],
   FirstFloor:[
     {
       floor:"First Floor",
-      square:  56500,
+      square: 58000,
       shop:"F1", 
-      shopFeet:714.12 ,
-      totalPrice: 40347780,
+      shopFeet:763.75 ,
+      totalPrice: 44297500,
       sold:true,
     },
     {
       floor:"First Floor",
-      square:56500,
+      square:58000,
       shop:"F2",
-      shopFeet:699.62,
-      totalPrice:39528530,
+      shopFeet:714.37,
+      totalPrice:41433460,
     },
     {
       floor:"First Floor",
-      square:71500,
+      square:58000,
       shop:"F3",
-      shopFeet:699.62,
-      totalPrice: 39528530,
+      shopFeet:714.37,
+      totalPrice: 41433460,
       sold:true,
     },
     {
       floor:"First Floor",
-      square:71500,
+      square:58000,
       shop:"F4",
-      shopFeet:670.62,
-      totalPrice: 37890030,
+      shopFeet:763.75,
+      totalPrice: 44297500,
     }
   ],
   SecondFloor:[
     {
       floor:"Second Floor",
-      square:  56500,
-      shop:"F1", 
-      shopFeet:714.12 ,
-      totalPrice: 40347780,
+      square: 58000,
+      shop:"S1", 
+      shopFeet:763.75 ,
+      totalPrice: 44297500,
       sold:true,
     },
     {
       floor:"Second Floor",
-      square:56500,
-      shop:"F2",
-      shopFeet:699.62,
-      totalPrice:39528530,
+      square:58000,
+      shop:"S2",
+      shopFeet:714.37,
+      totalPrice:41433460,
     },
     {
       floor:"Second Floor",
-      square:71500,
-      shop:"F3",
-      shopFeet:699.62,
-      totalPrice: 39528530,
+      square:58000,
+      shop:"S3",
+      shopFeet:714.37,
+      totalPrice: 41433460,
       sold:true,
     },
     {
       floor:"Second Floor",
-      square:71500,
-      shop:"F4",
-      shopFeet:670.62,
-      totalPrice: 37890030,
+      square:58000,
+      shop:"S4",
+      shopFeet:763.75,
+      totalPrice: 44297500,
     }
   ]
 }
